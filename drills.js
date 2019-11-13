6172f2030cadd23f306473d049377b52e97ee804
Smiti Shakya <smitishakya@Smitis-MacBook-Pro.local>
2019-11-13 16:19:34 -0600

    added BST

--- drills.js
+++ drills.js
@@ -16,3 +16,65 @@ our search value. So the search algorithm stops.
 When given the value of 16 to be searched, the function calculated the midpoint (11), and because the value is greater than the midpoint, the end index will continue to
 shift left until the value is found. Because the value does not exist in the array, the function will return a value of -1.
 */ 
+import BinarySearchTree from './binarysearch';
+
+let books = [
+  { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+  { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+  { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+  { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+  { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+  { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+  { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+  { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+  { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+  { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+];
+
+function deweySearch(books, dewey, title){
+    const keys = Object.keys(books[1]);
+    //console.log(keys);
+    books.forEach(book => {
+        //console.log(book[keys[1]] );
+        if(book[keys[1]] === dewey && book[keys[2]] === title){
+
+            //console.log(book);
+        }
+        
+    });
+
+  
+}
+
+deweySearch(books, '005.133', 'The REXX Language');
+function binarySearch(array, value, start, end) {
+    var start = start === undefined ? 0 : start;
+    var end = end === undefined ? array.length : end;
+
+    if (start > end) {
+        return -1;
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    console.log(start, end);
+    if (item == value) {
+        return index;
+    }
+    else if (item < value) {
+        return binarySearch(array, value, index + 1, end);
+    }
+    else if (item > value) {
+        return binarySearch(array, value, start, index - 1);
+    }
+};
+
+function treeTraversals(){
+    let BST = new BinarySearchTree();
+    BST.insert(25, 29);
+    BST.insert(15, 16);
+    console.log(BST);
+}
+
+//treeTraversals();
--- react-app/src/search.js
+++ react-app/src/search.js
@@ -5,30 +5,38 @@ class Search extends Component {
     super()
     this.state = {
       search: null,
-      array: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62,
-        93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 
-        13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6,
-        39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
+      counter: 0
     }
+    
   }
 
+  array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62,
+    93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 
+    13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6,
+    39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
 
-  linearSearch = (array, value, counter=0) => {
-    value = parseInt(value);
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === value) {
-        counter++
-        return i;
+
+  handlelinearSearch = (e) => {
+    //e.preventDefault();
+    let value = parseInt(this.state.search);
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        return `${value} found in ${i} times`;
       }
+     
     }
-    return -1;
+    
+    return `${value} not found in ${this.array.length} times`;
   }
 
-  //sortArray = this.state.array.sort((a,b) => a-b);
+  
+  sortArray = this.array.sort((a,b) => a-b);
 
-  binarySearch = (array, value, start, end) => {
-    value = parseInt(value);
+  binarySearch = (array = this.sortArray, value, start, end = this.sortArray.length-1) => {
+    
+    console.log(array);
+    value = parseInt(this.state.search);
     start = start === undefined ? 0 : start;
     end = end === undefined ? array.length : end;
 
@@ -44,34 +52,45 @@ class Search extends Component {
         return index;
     }
     else if (item < value) {
-        return this.binarySearch(this.state.array, value, index + 1, end);
+        return this.binarySearch(array, value, index + 1, end);
     }
     else if (item > value) {
-        return this.binarySearch(this.state.array, value, start, index - 1);
+        return this.binarySearch(array, value, start, index - 1);
     }
+    console.log(this.binarySearch())
   };
+  
+
+
 
   handleSubmit = e => {
     e.preventDefault();
 
+    
+    
     this.setState({
       search: e.target.search.value
     })
-    console.log(this.linearSearch(this.state.array, e.target.search.value, counter = 0))
-    //console.log(this.binarySearch(this.sortArray, e.target.search.value))
+    
+    console.log(this.handlelinearSearch())
+    
   }
 
   render() {
+    console.log(this.sortArray);
     return (
+      
       <div className="linear-search">
         <h1>
           Linear/Binary Search
         </h1>
         <form onSubmit={this.handleSubmit}>
           Search Value: 
-          <input type="text" id="search" name="search">
+          <input type="text" id="search" onChange={e => this.setState({ search: e.target.value })} name="search">
           </input>
-          <input type="submit" value="Submit"></input>
+          <button onClick={this.handlelinearSearch} value="Linear Search">Linear Search</button>
+          <button onClick={this.binarySearch} value="Binary Search">Binary Search</button>
+          <h2>{this.state.search}</h2>
         </form>
       </div>
     )
