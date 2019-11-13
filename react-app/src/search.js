6544f09ead06aec8d38853a4565abdca44d41c2d
Hubert Yang <offspring7890@yahoo.com>
2019-11-13 11:28:26 -0800

    working on search algo

--- react-app/src/search.js
+++ react-app/src/search.js
@@ -9,48 +9,45 @@ class Search extends Component {
         93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 
         13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6,
         39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
-      newArray: [],
     }
   }
 
-  linearSearch = (array, value) => {
+
+
+  linearSearch = (array, value, counter=0) => {
     value = parseInt(value);
     for (let i = 0; i < array.length; i++) {
       if (array[i] === value) {
+        counter++
         return i;
       }
     }
     return -1;
   }
 
-  sortArray = (array) => {
-    let newArray = array.sort((a,b) => a-b)
-    this.setState({ newArray })
-    console.log(newArray)
-    return;
-  }
+  //sortArray = this.state.array.sort((a,b) => a-b);
 
-  binarySearch = (newArray, value, start, end) => {
+  binarySearch = (array, value, start, end) => {
     value = parseInt(value);
     start = start === undefined ? 0 : start;
-    end = end === undefined ? newArray.length : end;
+    end = end === undefined ? array.length : end;
 
     if (start > end) {
       return -1;
     }
 
     const index = Math.floor((start + end) / 2);
-    const item = newArray[index];
+    const item = array[index];
 
     console.log(start, end);
     if (item === value) {
         return index;
     }
     else if (item < value) {
-        return this.binarySearch(newArray, value, index + 1, end);
+        return this.binarySearch(this.state.array, value, index + 1, end);
     }
     else if (item > value) {
-        return this.binarySearch(newArray, value, start, index - 1);
+        return this.binarySearch(this.state.array, value, start, index - 1);
     }
   };
 
@@ -60,9 +57,8 @@ class Search extends Component {
     this.setState({
       search: e.target.search.value
     })
-    console.log(this.linearSearch(this.state.array, e.target.search.value))
-    console.log(this.binarySearch(this.state.array, e.target.search.value))
-    console.log(this.sortArray(this.state.newArray))
+    console.log(this.linearSearch(this.state.array, e.target.search.value, counter = 0))
+    //console.log(this.binarySearch(this.sortArray, e.target.search.value))
   }
 
   render() {
